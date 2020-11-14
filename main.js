e42f2de5149998a91403fa6c2a4261ce05121856
Konstantin <picprofit@gmail.com>
2020-11-14 10:33:20 +0300

    add: l5 loops

--- main.js
+++ main.js
@@ -6,6 +6,24 @@ const app = Vue.createApp({
       image: './assets/images/socks_green.jpg',
       inventory: 2,
       onSale: true,
+      details: [
+        '50% cotton',
+        '50% wool',
+        '50% polyester',
+        '50% fakes'
+      ],
+      sizes: [
+        'S',
+        'M',
+        'L',
+        'XL',
+        'XXL',
+        'XXXL'
+      ],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' },
+      ],
       credits: {
         url: 'https://www.vuemastery.com/courses/intro-to-vue-3/',
         title: 'Intro to Vue 3'
