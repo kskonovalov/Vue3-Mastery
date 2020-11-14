67186d26a6e84f14d45c0b9916083d1ab51a2759
Konstantin <picprofit@gmail.com>
2020-11-14 10:18:15 +0300

    add: l3 image & bind attributes

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const app = Vue.createApp({
   data: function() {
     return {
       product: 'Vue3 Socks',
-      description: 'Vue3 test description'
+      description: 'Vue3 test description',
+      image: './assets/images/socks_green.jpg',
+      credits: {
+        url: 'https://www.vuemastery.com/courses/intro-to-vue-3/',
+        title: 'Intro to Vue 3'
+      },
     }
   }
 })
