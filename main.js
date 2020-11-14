bcea953261a40158517182d4498ecf7f9e0f814a
Konstantin <picprofit@gmail.com>
2020-11-14 10:25:30 +0300

    add: l4 conditionals

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const app = Vue.createApp({
       product: 'Vue3 Socks',
       description: 'Vue3 test description',
       image: './assets/images/socks_green.jpg',
+      inventory: 2,
+      onSale: true,
       credits: {
         url: 'https://www.vuemastery.com/courses/intro-to-vue-3/',
         title: 'Intro to Vue 3'
